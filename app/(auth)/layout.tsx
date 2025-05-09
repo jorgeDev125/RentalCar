20afa48d5bf79eaa77985b8da0fd7df3d1262401
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-05-09 11:29:27 -0500

    Add file structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+export default function AuthLayout({children}: {children: React.ReactNode}) {
+  return (
+    <div className="grid lg:grid-cols-2 h-full items-center justify-center">
+        <div className="flex items-center justify-center">{children}</div>
+        <div className="hidden lg:flex lg:bg-slate-300 h-full justify-center items-center lg:flex-col">
+            <Image 
+                height={100}
+                width={100}
+                src="/logo.svg"
+                alt="Logo"
+            />
+            <h1 className="text-xl font-bold">Horse´s Rental Cars</h1>
+        </div>
+
+    </div>
+  )
+}
---
+++ app/(routes)/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <div>From Home page</div>
+  )
+}
