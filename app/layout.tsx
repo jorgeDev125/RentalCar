f018004c84be8eb04ac5454cedc554458d829df2
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-05-09 11:28:24 -0500

    Install Clerk and settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
 import type { Metadata } from "next";
-import {Outfit } from "next/font/google";
+import { Outfit } from "next/font/google";
 import NextTopLoader from 'nextjs-toploader';
 import "./globals.css";
+import {
+  ClerkProvider,
+ /*  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton, */
+} from '@clerk/nextjs'
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -21,13 +29,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${outfit.variable} antialiased`}
-      >
-        <NextTopLoader color="#000" />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${outfit.variable} antialiased h-full`}
+        >
+          <NextTopLoader color="#000" />
+         {/*  <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header> */}
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
