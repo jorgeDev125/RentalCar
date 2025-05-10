52dbe4e61715a9f664665179d2156ec7bc23f69b
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-05-09 21:17:07 -0500

    Install prisma and connect data base

--- lib/db.ts
+++ lib/db.ts
@@ -8,15 +8,3 @@ declare global  {
 export const db = globalThis.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db
-
-/* import { PrismaClient } from '@prisma/client'
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  })
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma */
